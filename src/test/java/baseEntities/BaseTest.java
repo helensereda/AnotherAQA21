0c9a81b5d0b74decee6d2a99a6dfd33c9ed7fce6
helensereda <sereda.elenaby@gmail.com>
2023-09-03 21:43:02 +0300

    apitests

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -1,7 +1,30 @@
 package baseEntities;
 
+import factory.BrowserFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import steps.LoginStep;
+import utils.configuration.ReadProperties;
 
 public class BaseTest {
-    public static WebDriver driver;
+    protected WebDriver driver;
+
+    protected LoginStep loginStep;
+
+    @BeforeMethod
+    public void setUp() {
+        BrowserFactory browserFactory = new BrowserFactory();
+        driver = browserFactory.getDriver();
+
+        loginStep = new LoginStep(driver);
+
+        driver.get(ReadProperties.getUrl());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
